5ee2fee4b22c9727e992c6cfa3af7038cae3e8bd
SaadatZaman <34128144+SaadatZaman@users.noreply.github.com>
2017-12-14 00:47:37 +0200

    Add files via upload

---
+++ LevelLoader.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour {
+
+	private bool playerInZone;
+
+	public string levelToLoad;
+
+	void Start () {
+
+		playerInZone = false;
+	
+	}
+
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.KeypadEnter) && playerInZone) {
+			SceneManager.LoadScene (levelToLoad);
+		}
+	}
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if(other.name == "Player")
+		{
+			playerInZone = true;
+		}
+}
+
+	void OnTriggerExit2D(Collider2D other)
+	{
+		if(other.name == "Player")
+		{
+			playerInZone = false;
+		}
+	}
+}
